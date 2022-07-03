e36e470a4f938dd8f31828e540286440f6f33c42
SimonDMaxwell <lolicongress@outlook.com>
2022-07-03 07:47:53 -0500

    unnecessary middleware removed

--- backend/middlewares/shareware.js
+++
@@ -1,6 +0,0 @@
-const shareware = (req, res, next) => {
-    if (req.session.logtype != "accounts" && req.session.logtype != "supervisor") res.sendStatus(401);
-    else next();
-}
-
-module.exports = shareware;
